89e84a97cccf4070c23a5fb0ac75a917e6b8b4fd
Daniel Luis <danniellcontato@gmail.com>
2023-03-24 06:26:45 -0300

    Função logar, verificando usuario e senha

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 //
+const usuario = 'daniel';
+const acesso = 1234
+
 const btn = document.querySelector(".btn");
 
-window.onload = function(){
+/*window.onload = function(){
     console.log(btn);
+}*/
+
+function logar(){
+    var login = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == usuario && senha == acesso){
+        alert('Login realizado com sucesso');
+    }else{
+        alert('Usuario ou senha incorreto');
+    }
 }
+/*
+btn.addEventListener("click", () => {
+    logar();
+});*/
 
 btn.addEventListener("click", (e) => {
     e.preventDefault()
-    alert("Testando o botão")
+    //alert("Testando o botão")
+    logar();
 });
 
